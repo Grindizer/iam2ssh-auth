fea3d4b2f1c448538e27ebd97906bea9ef842ab7
nassim <nassim.babaci@cloudreach.co.uk>
2017-05-15 12:39:18 +0200

    Initial commit. Command line interface to manage iam user public ssh keys.

---
+++ iamauth/cli.py
@@ -0,0 +1,46 @@
+import sys
+from cliff.app import App
+from cliff.commandmanager import CommandManager
+from cliff.lister import Lister
+from core import get_authorized_keys
+
+
+class IamAuthApp(App):
+    def __init__(self):
+        super(IamAuthApp, self).__init__(
+            description='Manage ssh authorized keys based on aws iam users.',
+            version='0.1',
+            command_manager=CommandManager('cliff.iamauth.commands'),
+            deferred_help=True,
+        )
+
+
+class AuthorizedKeys(Lister):
+    formatter_default = 'value'
+
+    def get_parser(self, prog_name):
+        parser = super(AuthorizedKeys, self).get_parser(prog_name)
+        parser.add_argument('--group-name', '-g', help="The name of the group from which "
+                                                       "to get the public ssh keys", required=True, dest='group')
+        parser.add_argument('--source-account-role-arn', '-r', help="Thr ARN of the role to assume within the account "
+                                                                    "that manages the iam users", required=True,
+                            dest='role')
+        return parser
+
+    def take_action(self, args):
+        # import pdb; pdb.set_trace()
+        return (
+            ('UserName', 'SSHPublicKeyId', 'Status', 'SSHPublicKeyBody'),
+            (
+                (key['UserName'], key['SSHPublicKeyId'], key['Status'], key['SSHPublicKeyBody'])
+                for key in get_authorized_keys(args.group, args.role)
+            )
+        )
+
+
+def main(argv=sys.argv[1:]):
+    app = IamAuthApp()
+    return app.run(argv)
+
+if __name__ == '__main__':
+    main()
---
+++ iamauth/core.py
@@ -0,0 +1,38 @@
+import boto3
+from contextlib import contextmanager
+
+@contextmanager
+def assume_role(role, session_name="NewSession"):
+    """
+    Usage :
+        with assume_role("somerole") as readonly:
+            iam = readonly.client('iam')
+    """
+    client = boto3.client('sts')
+    response = client.assume_role(RoleArn=role, RoleSessionName=session_name)
+    yield boto3.Session(
+        aws_access_key_id=response['Credentials']['AccessKeyId'],
+        aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+        aws_session_token=response['Credentials']['SessionToken'])
+
+
+def list_ssh_public_keys_for_group(group_name, iam_session):
+    group = iam_session.get_group(GroupName=group_name)
+    for user in group['Users']:
+        user_name = user['UserName']
+        sshkeys = iam_session.list_ssh_public_keys(UserName=user_name)
+        for sshkey in sshkeys['SSHPublicKeys']:
+            if sshkey['Status'] == 'Active':
+                sshkey_id = sshkey['SSHPublicKeyId']
+                public_key = iam_session.get_ssh_public_key(UserName=user_name,
+                                                            SSHPublicKeyId=sshkey_id,
+                                                            Encoding='SSH')
+                yield public_key
+
+
+def get_authorized_keys(group_name, source_role):
+    with assume_role(source_role, 'authorizedkeysLookup') as iam_account:
+        iam_session = iam_account.client('iam')
+        for key in list_ssh_public_keys_for_group(group_name, iam_session):
+            if 'SSHPublicKey' in key:
+                yield key['SSHPublicKey']
